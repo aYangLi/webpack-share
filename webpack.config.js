ce7dc341bf7b19c322696feb41a538764c787a87
lichenyang <lichenyang@allinmd.cn>
2017-12-06 16:15:58 +0800

    添加一些注释

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const htmlWebpackPlugin = require("html-webpack-plugin"); //html插件，生成�
 var _dirname = "";
 module.exports = {
   // context:'',//整个运行环境的上下文默认为根目录
+  // entry 是配置页面的入口文件；
   // entry:'./src/script/main.js', //第一种方试
   // entry:['./src/script/main.js','./src/script/a.js'],//第二种打包方式，解决平行的互相依赖的打包到一起
   entry:{
@@ -9,7 +10,8 @@ module.exports = {
     a:'./src/script/a.js',
     b:'./src/script/b.js',
     c:'./src/script/c.js',
-  },//第三种方式，传一个对象， 上面两种方式的融合（多页面应用），都可以传（我们现在配置的就是这种方式 ）  
+  },//第三种方式，传一个对象， 上面两种方式的融合（多页面应用），都可以传（我们现在配置的就是这种方式 ）
+  // output 配置打包输出类相关； 
   output:{
     // filename:'bundle.js',//打包出来的文件名，前两种方式的输出名字，
     filename:'js/[name]-[hash].js',//第三种方式的输出；2以上强制要求多路径这种配置，1可以上面那种方式
@@ -17,13 +19,26 @@ module.exports = {
     // [name]指的是入口对象的key，[hash]本次打包的hash值，
     // [chuckhash]每个文件打包的哈希值，只有当每个文件发生变化的时候这个哈希值才变化；(对静态资源是比较有用的)
     // path: __dirname + '/dist/js/',
+    // path：打包输出目录
     path: __dirname + '/dist',
     publicPath:'http://cdn.com/',//打包生成的文件加上服务器地址，默认不加
   },
   //配置插件
   plugins:[
-    //生成js并且添加到html中，不传参数的话，默认在打包完成的目录下创建一个index.html文件并且引入打包完成的文件 
-    new htmlWebpackPlugin({
+    /**
+     * html-webpack-plugin用来打包入口html文件
+     * entry配置的入口是js文件, webpack以js文件为入口, 遇到import, 用配置的loader加载引入文件
+     * 但作为浏览器打开的入口html, 是引用入口js的文件, 它在整个编译过程的外面,
+     * 所以, 我们需要html-webpack-plugin来打包作为入口的html文件
+     */ 
+    new htmlWebpackPlugin({//生成js并且添加到html中，不传参数的话，默认在打包完成的目录下创建一个index.html文件并且引入打包完成的文件 
+      /**
+       * template参数指定入口html文件路径, 插件会把这个文件交给webpack去编译,
+       * webpack按照正常流程, 找到loaders中test条件匹配的loader来编译, 那么这里html-loader就是匹配的loader
+       * html-loader编译后产生的字符串, 会由html-webpack-plugin储存为html文件到输出目录, 默认文件名为index.html
+       * 可以通过filename参数指定输出的文件名
+       * html-webpack-plugin也可以不指定template参数, 它会使用默认的html模板.
+       */
       template:'index.html',//引入根目录下index.html模板，并将生成到的文件添加，重新在输出目录生成一个index.html
       filename:'a.html',//生成的文件名称；
       inject:'head',//插入到文件的部位（head， body）设置为false则不自动插入代码，根据需要手动在模板里面添加
--- webpack.config.module.js
+++ webpack.config.module.js
@@ -1,8 +1,10 @@
 const htmlWebpackPlugin = require("html-webpack-plugin"); //html插件，生成的文件插入到html中
-var path = require('path');
+var path = require('path');//path模块对路径的操作
 module.exports = {
   // context:'',//整个运行环境的上下文默认为根目录
+  // entry 是配置页面的入口文件；
   entry:'./src/app.js', //第一种方试 
+  // output 配置打包输出类相关；
   output:{
     // filename:'bundle.js',//打包出来的文件名，前两种方式的输出名字，
     filename:'js/[name].bundle.js',//第三种方式的输出；2以上强制要求多路径这种配置，1可以上面那种方式
@@ -10,12 +12,29 @@ module.exports = {
     // [name]指的是入口对象的key，[hash]本次打包的hash值，
     // [chuckhash]每个文件打包的哈希值，只有当每个文件发生变化的时候这个哈希值才变化；(对静态资源是比较有用的)
     // path: __dirname + '/dist/js/',
-    path: __dirname + '/dist',
+    path: __dirname + '/dist',// path：打包输出目录
   },
-  //配置插件
+  /**
+   * 配置插件
+   * plugin 和 loader 的区别是：
+   * loader 是import时 根据 不同的文件名，匹配不同的 loader 对这个文件做处理，
+   * plugin ，关注的不是文件的格式，而是在编译的各个阶段，会触发不同的事件，让开发者可以干预每个编译阶段；
+   */
   plugins:[
-    //生成js并且添加到html中，不传参数的话，默认在打包完成的目录下创建一个index.html文件并且引入打包完成的文件 
-    new htmlWebpackPlugin({
+    /**
+     * html-webpack-plugin用来打包入口html文件
+     * entry配置的入口是js文件, webpack以js文件为入口, 遇到import, 用配置的loader加载引入文件
+     * 但作为浏览器打开的入口html, 是引用入口js的文件, 它在整个编译过程的外面,
+     * 所以, 我们需要html-webpack-plugin来打包作为入口的html文件
+     */ 
+    new htmlWebpackPlugin({//生成js并且添加到html中，不传参数的话，默认在打包完成的目录下创建一个index.html文件并且引入打包完成的文件
+      /**
+       * template参数指定入口html文件路径, 插件会把这个文件交给webpack去编译,
+       * webpack按照正常流程, 找到loaders中test条件匹配的loader来编译, 那么这里html-loader就是匹配的loader
+       * html-loader编译后产生的字符串, 会由html-webpack-plugin储存为html文件到输出目录, 默认文件名为index.html
+       * 可以通过filename参数指定输出的文件名
+       * html-webpack-plugin也可以不指定template参数, 它会使用默认的html模板.
+       */
       template:'module.html',//引入根目录下index.html模板，并将生成到的文件添加，重新在输出目录生成一个index.html
       filename:'module.html',//生成的文件名称；
       inject:'body',//插入到文件的部位（head， body）设置为false则不自动插入代码，根据需要手动在模板里面添加
@@ -24,6 +43,10 @@ module.exports = {
   ],
   // 配置loader
   module:{
+    /*
+    配置各种文件的加载器，称之为loader
+    webpack 当遇到import ... 时，会调用这里配置的 loader 对引用的文件进行编译；
+     */
     rules:[
       {
         test:/\.js$/,
